554730d89a9307b7435aa3afcf7d69a7836b5bde
Ronald <jepunnoora@gmail.com>
2021-09-18 10:57:47 +0700

    modul2 ronaldgrant

--- ronaldJmartBO/Jmart.java
+++ ronaldJmartBO/Jmart.java
@@ -10,59 +10,68 @@ package ronaldJmartBO;
 public class Jmart
 {
     public static void main(String[] args) {
-        int before = 10000;
-        int after = 7000;
+        create();
+        // int before = 10000;
+        // int after = 7000;
         
-        System.out.println("Promo = " + getPromo());
-        System.out.println("Customer = " + getCustomer());
-        System.out.println("Discount Percentage = " + getDiscountPercentage(before, after));
-        System.out.println("Discounted Price = " + getDiscountedPrice(before, getDiscountPercentage(before, after)));
-        System.out.println("Original Price = " + getOriginalPrice(getDiscountedPrice(before, getDiscountPercentage(before, after)), getDiscountPercentage(before, after)));
-        System.out.println("Commission Multiplier = " + getCommissionMultiplier());
-        System.out.println("Adjusted Price = " + getAdjustedPrice(getDiscountedPrice(before, getDiscountPercentage(before, after))));
-        System.out.println("Admin Fee = " + getAdminFee(getDiscountedPrice(before, getDiscountPercentage(before, after))));
+        // System.out.println("Promo = " + getPromo());
+        // System.out.println("Customer = " + getCustomer());
+        // System.out.println("Discount Percentage = " + getDiscountPercentage(before, after));
+        // System.out.println("Discounted Price = " + getDiscountedPrice(before, getDiscountPercentage(before, after)));
+        // System.out.println("Original Price = " + getOriginalPrice(getDiscountedPrice(before, getDiscountPercentage(before, after)), getDiscountPercentage(before, after)));
+        // System.out.println("Commission Multiplier = " + getCommissionMultiplier());
+        // System.out.println("Adjusted Price = " + getAdjustedPrice(getDiscountedPrice(before, getDiscountPercentage(before, after))));
+        // System.out.println("Admin Fee = " + getAdminFee(getDiscountedPrice(before, getDiscountPercentage(before, after))));
     }
     
-    public static int getPromo() {
-        return 0;
+    public static Product create() {
+        // ProductCategory category = ProductCategory.GAMING;
+        // PriceTag priceTag = new PriceTag(10000, 50);
+        
+        // Product product = new Product("PS4", false, priceTag, category);
+        return new Product("PS4", 4, false, new PriceTag(10000, 50), ProductCategory.GAMING);
     }
     
-    public static String getCustomer() {
-        return "oop";
-    }
+    // public static int getPromo() {
+        // return 0;
+    // }
     
-    public static float getDiscountPercentage(int before, int after) {
-        int potonganHarga = before - after;
-        if (before < after) {
-            return 0.0f;
-        }
-        else {
-            return (float)(potonganHarga / 100);
-        }
-    }
+    // public static String getCustomer() {
+        // return "oop";
+    // }
     
-    public static int getDiscountedPrice(int price, float discountPercentage) {
-        if(discountPercentage > 100.0f) {
-            return 100;
-        }
-        else {
-            return (int)(price - (price * discountPercentage / 100));
-        }
-    }
+    // public static float getDiscountPercentage(int before, int after) {
+        // int potonganHarga = before - after;
+        // if (before < after) {
+            // return 0.0f;
+        // }
+        // else {
+            // return (float)(potonganHarga / 100);
+        // }
+    // }
     
-    public static int getOriginalPrice(int discountedPrice, float discountPercentage) {
-        return (int)(discountedPrice * 100 / (100 - discountPercentage));
-    }
+    // public static int getDiscountedPrice(int price, float discountPercentage) {
+        // if(discountPercentage > 100.0f) {
+            // return 100;
+        // }
+        // else {
+            // return (int)(price - (price * discountPercentage / 100));
+        // }
+    // }
     
-    public static float getCommissionMultiplier() {
-        return 0.05f;
-    }
+    // public static int getOriginalPrice(int discountedPrice, float discountPercentage) {
+        // return (int)(discountedPrice * 100 / (100 - discountPercentage));
+    // }
     
-    public static int getAdjustedPrice(int price) {
-        return price + (int)(price * getCommissionMultiplier());
-    }
+    // public static float getCommissionMultiplier() {
+        // return 0.05f;
+    // }
     
-    public static int getAdminFee(int price) {
-        return (int)(price * getCommissionMultiplier());
-    }
+    // public static int getAdjustedPrice(int price) {
+        // return price + (int)(price * getCommissionMultiplier());
+    // }
+    
+    // public static int getAdminFee(int price) {
+        // return (int)(price * getCommissionMultiplier());
+    // }
 }
